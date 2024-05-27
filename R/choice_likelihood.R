99e5f75d072ddf51623658e99fabc981e940b266
loelschlaeger <oelschlaeger.lennart@gmail.com>
2024-05-27 12:04:50 +0200

    new file for choice_probabilities

--- R/choice_likelihood.R
+++ R/choice_likelihood.R
@@ -14,10 +14,3 @@ choice_likelihood <- function(
 
 }
 
-
-choice_probabilities <- function(
-  choice_parameters, choice_data, choice_formula, choice_alternatives,
-  model_type = "probit"
-) {
-
-}
---
+++ R/choice_probabilities.R
@@ -0,0 +1,6 @@
+choice_probabilities <- function(
+    choice_parameters, choice_data, choice_formula, choice_alternatives,
+    model_type = "probit"
+) {
+
+}
